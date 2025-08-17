ac7da2ebb159321cd69ac6f4201a2281095c0a5c
Aditya <adityaydv12890@gmail.com>
2025-08-17 12:01:07 +0530

    shorten url login written

---
+++ src/app/api/url/shorten/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import { connect } from "@/dbConfig/dbConfig";
+import Url from "@/models/models";
+import { generateShortCode } from "@/utils/generateshortcode";
+
+export async function POST(req: Request) {
+    const { longUrl } = await req.json();
+
+    if(!longUrl) {
+        return NextResponse.json(
+            {error: "Missing longUrl"},
+            {status: 400}
+        )
+    }
+
+    await connect()
+
+    const shortCode = generateShortCode();
+
+    const newUrl = new Url({ shortCode, longUrl});
+    await newUrl.save();
+
+    return NextResponse.json({
+        shortUrl: `${process.env.PUBLIC_BASE_URL}/${shortCode}`
+    });
+}
